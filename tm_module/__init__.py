76f766111cdd8f6382c3432872d91123e34cc1cc
antonio258 <antonio258p@gmail.com>
2024-03-26 23:54:45 -0300

    hotfix

--- tm_module/__init__.py
+++ tm_module/__init__.py
@@ -1,4 +1,4 @@
-from .utils.reader import Reader
+from .utils import Reader
 from .cluwords import CluWords
 from .nmf import NMF
 from .bertopic import BERTopic
---
+++ tm_module/utils/__init__.py
@@ -0,0 +1,2 @@
+from .logger import Logger
+from .reader import Reader
